e16b8be706a067581b85ed23f0bbdc78aa11580b
Jason Zimdars <jz@37signals.com>
2025-04-14 09:32:06 -0500

    Attempt to fix notifications index

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,10 @@
 class NotificationsController < ApplicationController
   def index
-    set_page_and_extract_portion_from Current.user.notifications.read.ordered
-
-    if @page.first?
+    if @page&.first?
       @unread = Current.user.notifications.unread.ordered
     end
+
+    set_page_and_extract_portion_from Current.user.notifications.read.ordered
   end
 
   def mark_read
