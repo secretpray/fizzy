eec953d8005579b603792cf7ace5448629d1922d
Jason Zimdars <jz@37signals.com>
2025-05-22 16:50:30 -0500

    Assign a random color to new stages

--- app/models/workflow/stage.rb
+++ app/models/workflow/stage.rb
@@ -1,3 +1,10 @@
 class Workflow::Stage < ApplicationRecord
   belongs_to :workflow
+
+  before_validation :assign_random_color, on: :create
+
+  private
+    def assign_random_color
+      self.color ||= Card::Colored::COLORS.sample
+    end
 end
