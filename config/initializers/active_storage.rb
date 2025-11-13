11a38a4526a00a3c062fc46acb3afaa266a7f386
Kevin McConnell <kevin@37signals.com>
2025-11-13 11:43:40 +0000

    Initial (wip) staging deployment
    This adds an initial configuration for staging. Note that we are doing
    this ahead of having the full infrastructure in all 3 DCs. So this
    this will result in some cross-DC writes for now (e.g. we have a single cache in
    IAD). We'll correct this as the infrastructure becomes available.
    
    We'll also run jobs on every app server, until we split them out to
    separate instances.

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -3,3 +3,8 @@ ActiveSupport.on_load(:active_storage_blob) do
     expires_in 5.minutes, public: true
   end
 end
+
+# Use DB read/write splitting for Active Storage models
+ActiveSupport.on_load(:active_storage_record) do
+  connects_to database: { writing: :primary, reading: :replica }
+end
--- script/configure-lb-staging.sh
+++ script/configure-lb-staging.sh
@@ -4,66 +4,44 @@ set -e
 
 # fizzy-staging-lb-01.sc-chi-int.37signals.com
 #
-#   Service      Host                         Path    Target                                                                                       State    TLS
-#   fizzy        fizzy.37signals-staging.com  /       fizzy-staging-app-01.sc-chi-int.37signals.com,fizzy-staging-app-02.sc-chi-int.37signals.com  running  yes
-#   fizzy-admin  fizzy.37signals-staging.com  /admin  fizzy-staging-app-01.sc-chi-int.37signals.com                                                running  yes
 ssh app@fizzy-staging-lb-01.sc-chi-int.37signals.com \
   docker exec fizzy-load-balancer \
     kamal-proxy deploy fizzy \
+      --force \
       --tls \
       --host=fizzy.37signals-staging.com \
-      --target=fizzy-staging-app-01.sc-chi-int.37signals.com \
-      --read-target=fizzy-staging-app-02.sc-chi-int.37signals.com \
-      --tls-acme-cache-path=/certificates
-
-ssh app@fizzy-staging-lb-01.sc-chi-int.37signals.com \
-  docker exec fizzy-load-balancer \
-    kamal-proxy deploy fizzy-admin \
-      --host=fizzy.37signals-staging.com \
-      --path-prefix /admin \
-      --strip-path-prefix=false \
-      --target=fizzy-staging-app-01.sc-chi-int.37signals.com
+      --writer-affinity-timeout=1s \
+      --tls-acme-cache-path=/certificates \
+      --target=fizzy-staging-app-101.df-iad-int.37signals.com \
+      --target=fizzy-staging-app-102.df-iad-int.37signals.com \
+      --read-target=fizzy-staging-app-01.sc-chi-int.37signals.com \
+      --read-target=fizzy-staging-app-02.sc-chi-int.37signals.com
 
 # fizzy-staging-lb-101.df-iad-int.37signals.com
 #
-#   Service      Host                         Path    Target                                                                                        State    TLS
-#   fizzy        fizzy.37signals-staging.com  /       fizzy-staging-app-01.sc-chi-int.37signals.com,fizzy-staging-app-102.df-iad-int.37signals.com  running  yes
-#   fizzy-admin  fizzy.37signals-staging.com  /admin  fizzy-staging-app-01.sc-chi-int.37signals.com                                                 running  yes
 ssh app@fizzy-staging-lb-101.df-iad-int.37signals.com \
   docker exec fizzy-load-balancer \
     kamal-proxy deploy fizzy \
+      --force \
       --tls \
       --host=fizzy.37signals-staging.com \
-      --target=fizzy-staging-app-01.sc-chi-int.37signals.com \
-      --read-target=fizzy-staging-app-102.df-iad-int.37signals.com \
-      --tls-acme-cache-path=/certificates
-
-ssh app@fizzy-staging-lb-101.df-iad-int.37signals.com \
-  docker exec fizzy-load-balancer \
-    kamal-proxy deploy fizzy-admin \
-      --host=fizzy.37signals-staging.com \
-      --path-prefix /admin \
-      --strip-path-prefix=false \
-      --target=fizzy-staging-app-01.sc-chi-int.37signals.com
+      --writer-affinity-timeout=1s \
+      --tls-acme-cache-path=/certificates \
+      --target=fizzy-staging-app-101.df-iad-int.37signals.com \
+      --target=fizzy-staging-app-102.df-iad-int.37signals.com
 
 # fizzy-staging-lb-401.df-ams-int.37signals.com
 #
-#   Service      Host                         Path    Target                                                                                        State    TLS
-#   fizzy        fizzy.37signals-staging.com  /       fizzy-staging-app-01.sc-chi-int.37signals.com,fizzy-staging-app-402.df-ams-int.37signals.com  running  yes
-#   fizzy-admin  fizzy.37signals-staging.com  /admin  fizzy-staging-app-01.sc-chi-int.37signals.com                                                 running  yes
 ssh app@fizzy-staging-lb-401.df-ams-int.37signals.com \
   docker exec fizzy-load-balancer \
     kamal-proxy deploy fizzy \
+      --force \
       --tls \
       --host=fizzy.37signals-staging.com \
-      --target=fizzy-staging-app-01.sc-chi-int.37signals.com \
-      --read-target=fizzy-staging-app-402.df-ams-int.37signals.com \
-      --tls-acme-cache-path=/certificates
+      --writer-affinity-timeout=1s \
+      --tls-acme-cache-path=/certificates \
+      --target=fizzy-staging-app-101.df-iad-int.37signals.com \
+      --target=fizzy-staging-app-102.df-iad-int.37signals.com \
+      --read-target=fizzy-staging-app-401.df-ams-int.37signals.com \
+      --read-target=fizzy-staging-app-402.df-ams-int.37signals.com 
 
-ssh app@fizzy-staging-lb-401.df-ams-int.37signals.com \
-  docker exec fizzy-load-balancer \
-    kamal-proxy deploy fizzy-admin \
-      --host=fizzy.37signals-staging.com \
-      --path-prefix /admin \
-      --strip-path-prefix=false \
-      --target=fizzy-staging-app-01.sc-chi-int.37signals.com
