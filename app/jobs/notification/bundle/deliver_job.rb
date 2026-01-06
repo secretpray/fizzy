8a4b1447b1b07ed14c32316074a56fe34cca39ff
Stanko K.R. <stanko@stanko.io>
2026-01-06 09:44:20 +0100

    Add a blank line between queue_as and discard_on

--- app/jobs/notification/bundle/deliver_job.rb
+++ app/jobs/notification/bundle/deliver_job.rb
@@ -2,6 +2,7 @@ class Notification::Bundle::DeliverJob < ApplicationJob
   include SmtpDeliveryErrorHandling
 
   queue_as :backend
+
   discard_on ActiveJob::DeserializationError
 
   def perform(bundle)
