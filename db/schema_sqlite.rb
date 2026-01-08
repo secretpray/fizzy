2f700c42a4f5586e112559af03eb7d9498a2b442
Stanko K.R. <stanko@stanko.io>
2026-01-08 12:15:24 +0100

    Migrate SQLite

--- db/schema_sqlite.rb
+++ db/schema_sqlite.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.2].define(version: 2025_12_19_120755) do
+ActiveRecord::Schema[8.2].define(version: 2025_12_24_092315) do
   create_table "accesses", id: :uuid, force: :cascade do |t|
     t.datetime "accessed_at"
     t.uuid "account_id", null: false
